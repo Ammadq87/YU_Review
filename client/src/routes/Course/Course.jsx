80436a70b7dae8c5acab954a15480810c538daeb
Ammad <ammadq87@my.yorku.ca>
2023-06-13 00:33:13 -0400

    added banner component and fixed search bar link issue

--- client/src/routes/Course/Course.jsx
+++ client/src/routes/Course/Course.jsx
@@ -1,8 +1,25 @@
+import './Course.css';
+import Banner from "../../components/Banner/Banner"
 import ReviewDisplay from "../../components/ReviewDisplay/ReviewDisplay"
+
+const bannerConfig = {
+    title: convertCourseUrlToBannerTitle(location.href),
+    subTitle: 'Software Design'
+}
+
 export default function Course () {
     return (
         <div className="Course">
-            <ReviewDisplay/>
+            <Banner data={bannerConfig}/>
+            <div className="CourseReviews">
+                <ReviewDisplay/>
+            </div>
         </div>
     )
+}
+
+function convertCourseUrlToBannerTitle(url) {
+    const _url = url.split('/'); 
+    const _courseCode = _url[_url.length-1].split('-');
+    return `${_courseCode[1]} ${_courseCode[2]}` 
 }
